c77948215e8afa347ea90cf88a411b4486ddbed2
Eric Wen <a071232002@gmail.com>
2023-10-23 03:05:45 +0800

    hw4 ver.1

---
+++ src/hw4/Hw4.java
@@ -0,0 +1,95 @@
+package hw4;
+
+import java.util.Scanner;
+
+public class Hw4 {
+
+	public static void main(String[] args) {
+//		有個一維陣列如下：
+//		{29, 100, 39, 41, 50, 8, 66, 77, 95, 15}
+//		請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
+
+		Hw4db q1 = new Hw4db(29, 100, 39, 41, 50, 8, 66, 77, 95, 15);
+
+		System.out.println("平均值 = " + q1.getAvg());
+		q1.showOverAvg();
+		System.out.println("=============================");
+
+//		請建立一個字串，經過程式執行後，輸入結果是反過來的
+//		例如String s = “Hello World”，執行結果即為dlroW olleH
+
+		Hw4db q2 = new Hw4db("Hello World");
+		q2.showReverse();
+		System.out.println("=============================");
+
+//		有個字串陣列如下 (八大行星)：
+//		{“mercury”, “venus”, “earth”, “mars”, “jupiter”, “saturn”, “uranus”, “neptune”}
+//		請用程式計算出這陣列裡面共有多少個母音(a, e, i, o, u) 
+
+		Hw4db q3 = new Hw4db("mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune");
+		System.out.println("母音共有" + q3.getVowelCheck() + "個");
+		System.out.println("=============================");
+
+//		阿文上班時忘了帶錢包,想要向同事借錢,和他交情比較好的同事共有 5 個,其員工編號與身上現金列
+//		表如下：
+//		員工編號	25		32		8		19		27
+//		身上現金	2500	800		500		1000	1200
+
+//		請設計一個程式,可以讓小華輸入欲借的金額後,便會顯示哪些員工編號的同事
+//		有錢可借他;並且統計有錢可借的總人數:例如輸入 1000 就顯示「有錢可借的
+//		員工編號: 25 19 27 共 3 人!」
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("小華你想要多少？");
+
+		int money = sc.nextInt();
+		Hw4db q4 = new Hw4db(money);
+
+		q4.setEmpno(25, 32, 8, 19, 27);
+		q4.setCash(2500, 800, 500, 1000, 1200);
+//		現金資訊比對用
+//		q4.showCashTable();
+		q4.moneyWhere();
+		System.out.println("=============================");
+
+//		請設計由鍵盤輸入三個整數，分別代表西元yyyy年，mm月，dd日，執行後會顯示是該年的第幾天
+//		例：輸入 1984 9 8 三個號碼後，程式會顯示「輸入的日期為該年第252天」
+//		提示2：需將閏年條件加入)
+//		(提示3：擋下錯誤輸入：例如月份輸入為2，則日期不該超過29)
+
+		System.out.println("請輸入西元年");
+		int year = sc.nextInt();
+		System.out.println("請輸入月份");
+		int month = sc.nextInt();
+		System.out.println("請輸入日期");
+		int day = sc.nextInt();
+
+		Hw4db q5 = new Hw4db(year, month, day);
+		q5.dayOfYear();
+		System.out.println("=============================");
+		
+//		班上有8位同學，他們進行了6次考試結果如下：
+//			1	2	3	4	5	6	7	8
+//		1考	10	35	40	100	90	85	75	70
+//		2考	37	75	77	89	64	75	70	95
+//		3考	100	70	79	90	75	70	79	90
+//		4考	77	95	70	89	60	75	85	89
+//		5考	98	70	89	90	75	90	89	90
+//		6考	90	80	100	75	50	20	99	75
+//		請算出每位同學考最高分的次數
+
+		Hw4db q6 = new Hw4db(6, 8);
+		q6.setTestScore(1, 10, 35, 40, 100, 90, 85, 75, 70);
+		q6.setTestScore(2, 37, 75, 77, 89, 64, 75, 70, 95);
+		q6.setTestScore(3, 100, 70, 79, 90, 75, 70, 79, 90);
+		q6.setTestScore(4, 77, 95, 70, 89, 60, 75, 85, 89);
+		q6.setTestScore(5, 98, 70, 89, 90, 75, 90, 89, 90);
+		q6.setTestScore(6, 90, 80, 100, 75, 50, 20, 99, 75);
+//		分數表確認用
+//		q6.showScoreTable();
+		q6.showTimesTopOne();
+		
+		sc.close();
+	}
+}
---
+++ src/hw4/Hw4db.java
@@ -0,0 +1,231 @@
+package hw4;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Hw4db {
+
+	Scanner sc = new Scanner(System.in);
+
+//	Q1
+	private int[] array;
+	private double numAvg;
+//	Q2
+	private String word;
+//	Q3
+	private String[] wordXXX;
+//	Q4
+	private int moneyYouWant;
+	private int[][] moneyInfo;
+//	Q5
+	private int year;
+	private int month;
+	private int day;
+	private int[] daysInMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//	Q6
+	private int[][] scoreTable;
+	private int[][] scoreSort = new int[6][8];
+
+//	Q1**************************************************
+	public Hw4db(int... c) {
+		array = new int[c.length];
+		for (int i = 0; i < c.length; i++) {
+			array[i] = c[i];
+		}
+	}
+
+	public double getAvg() {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		numAvg = sum / array.length;
+		return numAvg;
+	}
+
+	public void showOverAvg() {
+		System.out.println("大於平均數的元素:");
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > numAvg) {
+				System.out.print(array[i] + " ");
+			}
+		}
+		System.out.println();
+	}
+
+//	Q2***************************************************
+
+	public Hw4db(String s) {
+		this.word = s;
+	}
+
+	public void showReverse() {
+		for (int i = word.length() - 1; i >= 0; i--) {
+			System.out.print(word.charAt(i));
+		}
+		System.out.println();
+	}
+
+//	Q3***************************************************
+
+	public Hw4db(String... s) {
+		wordXXX = new String[s.length];
+		for (int i = 0; i < s.length; i++) {
+			wordXXX[i] = s[i];
+		}
+	}
+
+	public int getVowelCheck() {
+
+		int count = 0;
+		for (int i = 0; i < wordXXX.length; i++) {
+			for (int j = 0; j < wordXXX[i].length(); j++) {
+				char w = wordXXX[i].charAt(j);
+				if (w == 'a' || w == 'e' || w == 'i' || w == 'o' || w == 'u') {
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+//	Q4****************************************************************************
+
+	public Hw4db(int money) {
+		this.moneyYouWant = money;
+	}
+
+	public void setEmpno(int... empNo) {
+		moneyInfo = new int[2][empNo.length];
+		for (int i = 0; i < empNo.length; i++) {
+			moneyInfo[0][i] = empNo[i];
+		}
+	}
+
+	public void setCash(int... cash) {
+		for (int i = 0; i < cash.length; i++) {
+			moneyInfo[1][i] = cash[i];
+		}
+	}
+//	現金資訊比對用
+	public void showCashTable() {
+		for (int j = 0; j < 2; j++) {
+			for (int i = 0; i < 5; i++) {
+				System.out.print(moneyInfo[j][i] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public void moneyWhere() {
+		int count = 0;
+		System.out.print("有錢可借的員工編號: ");
+		for (int i = 0; i < 5; i++) {
+			if (moneyInfo[1][i] >= moneyYouWant) {
+				count++;
+				System.out.print(moneyInfo[0][i] + " ");
+			}
+		}
+		System.out.print("共 " + count + " 人!");
+		System.out.println();
+	}
+
+//	Q5**************************************************************************
+
+	public Hw4db(int year, int month, int day) {
+		if (year % 4 == 0) {
+			daysInMonth[2] = 29;
+		}
+		while (true) {
+			if (day <= daysInMonth[month]) {
+				break;
+			}
+			System.out.println("Error 日期大於該月份天數, 請重新輸入");
+			System.out.println("請輸入西元年");
+			year = sc.nextInt();
+			System.out.println("請輸入月份");
+			month = sc.nextInt();
+			System.out.println("請輸入日期");
+			day = sc.nextInt();
+		}
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public void dayOfYear() {
+		int sum = 0;
+		for (int i = 0; i < month; i++) {
+			sum += daysInMonth[i];
+		}
+		sum += day;
+		System.out.println("輸入的日期為該年第" + sum + "天");
+	}
+
+//	Q6****************************************************************************
+
+	public Hw4db(int testNo, int studentNo) {
+		scoreTable = new int[testNo][studentNo];
+	}
+
+	public void setTestScore(int testNo, int... score) {
+		for (int i = 0; i < score.length; i++) {
+			scoreTable[testNo - 1][i] = score[i];
+		}
+	}
+//		分數表比對確認
+	public void showScoreTable() {
+//		原始分數表(為排序)
+		for (int j = 0; j < 6; j++) {
+			for (int i = 0; i < 8; i++) {
+				System.out.print(scoreTable[j][i] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("==========================================================");
+		
+//		分數排序驗證
+		for (int i = 0; i < 6; i++) {
+			scoreSort[i] = Arrays.copyOf(scoreTable[i], scoreTable[i].length);
+		}
+		for (int i = 0; i < 6; i++) {
+			Arrays.sort(scoreSort[i]);
+		}
+		for (int j = 0; j < 6; j++) {
+			for (int i = 0; i < 8; i++) {
+				System.out.print(scoreSort[j][i] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("==========================================================");
+	}
+	
+
+	public void showTimesTopOne() {
+//		複製一個新的二維陣列並做排序
+		for (int i = 0; i < 6; i++) {
+			scoreSort[i] = Arrays.copyOf(scoreTable[i], scoreTable[i].length);
+		}
+		for (int i = 0; i < 6; i++) {
+			Arrays.sort(scoreSort[i]);
+		}
+//		分數表每Row若=最高分(排序後index的對應值), 則對應號碼同學times+1
+		int[] times = new int[8];
+		for (int i = 0; i < 6; i++) {
+			for (int setNo = 0; setNo < 8; setNo++) {
+				if (scoreTable[i][setNo] == scoreSort[i][7]) {
+					times[setNo]++;
+				}
+			}
+		}
+		for (int i=1;i<=8;i++) {
+			System.out.print(i+"號"+"\t");	
+		}
+		System.out.println();
+		for (int i=0;i<8;i++) {
+			System.out.print(times[i]+"次"+"\t");
+		}
+	}
+
+}
